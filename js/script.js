e4d849aafc5036086d98defad4ff0c479dafe21d
Andrea-Sgura <andreasgura96@gmail.com>
2025-03-26 08:37:36 +0100

    [UPD] Eseguito corpo del programma, esercizio finito

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 // Recupero degli elementi del DOM e/o dichiarazione delle variabili
 const lampadina = document.getElementById("lampadina");
 const bottone = document.getElementById("bottone");
+
+// Corpo del programmma
+bottone.addEventListener("click", function(){
+    if (lampadina.src.includes("white_lamp.png")){
+        lampadina.src = "img/yellow_lamp.png";
+        bottone.textContent = "Spegni";
+    }
+    else{
+        lampadina.src = "img/white_lamp.png";
+        bottone.textContent = "Accendi";
+    }
+})
